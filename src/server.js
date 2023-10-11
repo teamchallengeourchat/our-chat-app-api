cc63f8648756ba1daa8ebf3610119a17a57110d2
Roman lesyo <ladiginscormag@gmail.com>
2023-10-11 12:41:53 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(
   cors({
-    origin: ['https://our-chat-app-two.vercel.app', 'http://localhost:3000', 'http://localhost:3001'],
+    origin: '*',
     credentials: true,
     optionsSuccessStatus: 200,
   })
@@ -52,7 +52,7 @@ app.use(errorsMidleware);
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ['https://our-chat-app-two.vercel.app', 'http://localhost:3000', 'http://localhost:3001'],
+    origin: '*',
     optionsSuccessStatus: 200,
   },
 });
