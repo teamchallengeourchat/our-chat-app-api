dd8f78fa7b050f4d5d8d8e927876b3f67925f126
Mifaresss <9803211@gmail.com>
2023-10-16 06:18:12 +0300

    Added privateChats

--- src/services/privateServices.js
+++ src/services/privateServices.js
@@ -1,62 +1,58 @@
-import { PrivatesList, PrivateMessages } from "../models/userPrivate/index.js";
+import { PrivatesList, PrivateMessages } from "../models/userPrivate/index.js"
 
 /**
- * 
- * @param {String} userId 
+ *
+ * @param {String} userId
  * @returns {[{ id: String, title: String }]} Prepared list of private messages
  */
 async function getChats(userId) {
-  
-	let List = await PrivatesList.find({users: { $in: userId}}).populate('users');
-	
-	List = List.map(({ _id, users }) => ({ id: _id.toString(), title: users.map(user => user.userName).join(", ") }));
-  
-  return List;
+	let List = await PrivatesList.find({ users: { $in: userId } }).populate("users")
+
+	List = List.map(({ _id, users }) => ({ id: _id.toString(), title: users.map(user => user.userName).join(", ") }))
+
+	return List
 }
 
 /**
- * 
- * @param {String} chatId 
+ *
+ * @param {String} chatId
  * @returns {[{ id: String, author: { id: String, name: String }, message: String, createdAt: Date }]} Prepared list of messages;
  */
 async function getChatHistory(chatId) {
-  const messages = await PrivateMessages.find({ chatId }).populate('author');
+	const messages = await PrivateMessages.find({ chatId }).populate("author")
 
-  const preparedMessages = messages.map(({ _id, author, message, createdAt }) => ({
-    id: _id.toString(),
-    author: {
-      id: author._id.toString(),
-      name: author.userName
-    },
-    message,
-    createdAt,
-  }));
+	// const preparedMessages = messages.map(({ _id, author, message, createdAt }) => ({
+	// 	id: _id.toString(),
+	// 	author: author,
+	// 	message,
+	// 	createdAt,
+	// }))
 
-  return preparedMessages;
+	return messages
 }
 
 /**
- * 
- * @param {String} chat_id 
- * @param {String} user_id 
+ *
+ * @param {String} chat_id
+ * @param {String} user_id
  * @returns {Boolean} if successful return true
  */
 async function leaveChat(chat_id, user_id) {
-  try {
-    const chatRoom = await PrivatesList.findById(chat_id);
-    chatRoom.users = chatRoom.users.filter(id => id.toString() !== user_id);
-    
-    if (chatRoom.users.length > 0) await chatRoom.save();
-    else await PrivatesList.findByIdAndDelete(chat_id);
-  } catch (error) {
-    throw new Error(error);
-  }
+	try {
+		const chatRoom = await PrivatesList.findById(chat_id)
+		chatRoom.users = chatRoom.users.filter(id => id.toString() !== user_id)
 
-  return true;
+		if (chatRoom.users.length > 0) await chatRoom.save()
+		else await PrivatesList.findByIdAndDelete(chat_id)
+	} catch (error) {
+		throw new Error(error)
+	}
+
+	return true
 }
 
 export default {
-  getChats,
-  getChatHistory,
-  leaveChat,
-};
+	getChats,
+	getChatHistory,
+	leaveChat,
+}
